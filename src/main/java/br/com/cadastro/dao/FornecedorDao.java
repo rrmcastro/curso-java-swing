a381babe0109a4e96c26ef8078085a694fd22da9
Ricardo Roméro Maranhão Castro <33225@PROWAY.TREINA>
2020-06-21 16:10:24 -0300

    atualizado pacote dao

---
+++ src/main/java/br/com/cadastro/dao/FornecedorDao.java
@@ -0,0 +1,20 @@
+package br.com.cadastro.dao;
+
+public class FornecedorDao {
+
+	public void cadastrar() {
+		
+	}
+	
+	public void listar() {
+		
+	}
+	
+	public void alterar() {
+		
+	}
+	
+	public void excluir() {
+		
+	}
+}
